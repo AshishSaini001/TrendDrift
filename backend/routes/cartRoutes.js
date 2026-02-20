a0596afc37e78631c89ca6139b353a3bb041e2ee
AshishSaini001 <02911.ashish@gmail.com>
2026-02-20 15:04:08 +0530

    implement get,put and delete product from cart api

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -4,6 +4,7 @@ const Product = require("../models/product");
 const User = require("../models/User");
 const { protect } = require("../middleware/authMiddleware");
 const cart = require("../models/cart");
+const { route } = require("./userRoutes");
 
 const router = express.Router();
 
@@ -84,6 +85,95 @@ router.post("/", async (req, res) => {
     }
   } catch (err) {
     console.log("Error adding to cart:", err.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+//@route PUT /api/cart
+//@desc Update cart item quantity or remove item
+//@access Public
+router.put("/", async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId.toString() &&
+        p.size === size &&
+        p.color === color,
+    );
+    if (productIndex > -1) {
+      if (quantity > 0) {
+        cart.products[productIndex].quantity = quantity;
+      } else {
+        cart.products.splice(productIndex, 1);
+      }
+      //Recalculate total Price
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (err) {
+    console.log("Error updating cart:", err.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+// @route GET /api/cart
+// @desc Get cart for guest or authenticated user
+// @access Public
+
+router.get("/", async (req, res) => {
+  const { guestId, userId } = req.query;
+  try {
+    const cart = await getCart(userId, guestId);
+    if (cart) {
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Cart is Empty" });
+    }
+  } catch (err) {
+    console.log("Error fetching cart:", err.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+// @route DELETE /api/cart
+// @desc remove a product for guest or authenticated user
+// @access Public
+
+router.delete("/", async (req, res) => {
+  const { productId, size, color, guestId, userId } = req.body;
+  try {
+    let cart = await getCart(userId, guestId);
+    if (cart) {
+      cart.products = cart.products.filter(
+        (p) =>
+          !(
+            p.productId.toString() === productId.toString() &&
+            p.size === size &&
+            p.color === color
+          ),
+      );
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      return res.status(200).json({ message: "Product removed from cart" });
+    } else {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+  } catch (err) {
+    console.log("Error clearing cart:", err.message);
     return res.status(500).json({ message: "Server error" });
   }
 });
